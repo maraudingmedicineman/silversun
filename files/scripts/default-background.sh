c0a1441d8ac973bbc3078c1dc88dacd36758cf80
maraudingmedicineman <anaaronboy1991@protonmail.com>
2026-05-28 10:51:18 -0500

    Initial gschema-overrides(4)

--- files/scripts/default-background.sh
+++ files/scripts/default-background.sh
@@ -3,7 +3,9 @@ set -eoux pipefail
 
 FEDORA_RELEASE="f$(rpm -E %fedora)"
 
-cat <<- "EOF" > /usr/share/glib-2.0/schemas/zz0-silversun.gschema.override
+echo -e "\033[1;33mSetting default background...\033[0m"
+
+cat <<- EOF > /usr/share/glib-2.0/schemas/zz0-silversun.gschema.override
 [org.gnome.desktop.background]
 picture-uri='file:///usr/share/backgrounds/${FEDORA_RELEASE}/default/${FEDORA_RELEASE}.xml'
 picture-uri-dark='file:///usr/share/backgrounds/${FEDORA_RELEASE}/default/${FEDORA_RELEASE}.xml'
