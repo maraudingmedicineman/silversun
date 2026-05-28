9aa4e99ecb0fac008d126737b7094e4299d6a594
maraudingmedicineman <anaaronboy1991@protonmail.com>
2026-05-28 10:10:09 -0500

    Initial gschema-overrides

---
+++ files/scripts/default-background.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eoux pipefail
+
+cat <<- "EOF" > /usr/share/glib-2.0/schemas/zz0-silversun.override
+[org.gnome.desktop.background]
+picture-uri='file:///usr/share/backgrounds/f$(rpm -E %fedora)/default/f$(rpm -E %fedora).xml'
+picture-uri-dark='file:///usr/share/backgrounds/f$(rpm -E %fedora)/default/f$(rpm -E %fedora).xml'
+
+[org.gnome.screensaver]
+picture-uri='file:///usr/share/backgrounds/f$(rpm -E %fedora)/default/f$(rpm -E %fedora).xml'
+EOF
